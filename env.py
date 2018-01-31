be6789b2652f8d8430fc339a7a7af9e69c825033
Takahiro Ishikawa <takahish.nil@gmail.com>
2018-01-31 17:58:25 +0900

    Added rest argument handling

--- env.py
+++ env.py
@@ -5,13 +5,13 @@ class _Env(dict):
     """An environment: a dict of {'var': val} pairs, with an outer Env.
     """
     def __init__(self, params=(), args=(), outer=None):
-        self.update(zip(params, args))
-        self.outer = outer
-
         # Bind param list to corresponding args, or single param to list of args
+        self.outer = outer
         if isinstance(params, symbol._Symbol):
             self.update({params: list(args)})
         else:
+            # bind rest parameters for lambda
+            params, args = _Env._parse_rest_argument(params, args)
             if len(args) != len(params):
                 raise TypeError('expected %s, given %s, ' % (params, args))
             self.update(zip(params, args))
@@ -25,6 +25,19 @@ class _Env(dict):
             raise LookupError(var)
         else:
             return self.outer.find(var)
+
+    @staticmethod
+    def _parse_rest_argument(params, args):
+        """Fine rest argument to parse params and args.
+        """
+        if '.' in params:
+            params, args = list(params), list(args) # for slicing and appending
+            # params=['x', '.', 'y'], args=[1, 2, 3, 4, 5]
+            #  => params=['x', 'y'], args=[1, [2, 3, 4, 5]]
+            rest_index = params.index('.')
+            params = params[:rest_index] + params[rest_index+1:]
+            args = args[:rest_index] + [args[rest_index:]]
+        return params, args
 
 _global_env = _Env()
 _global_env.update(ops._builtin_operator)
--- test/test_env.py
+++ test/test_env.py
@@ -4,6 +4,13 @@ import env
 import operator as op
 
 class UnitTestCase(unittest.TestCase):
+    def testEnv_parse_rest_argument(self):
+        params = ['x', '.', 'y']
+        args = [1, 2, 3, 4, 5]
+        params, args = env._Env._parse_rest_argument(params, args)
+        self.assertEqual(params, ['x', 'y'])
+        self.assertEqual(args, [1, [2, 3, 4, 5]])
+
     def testEnv(self):
         param = symbol._Symbol('a')
         args = [2, 3, 4]
@@ -32,6 +39,12 @@ class UnitTestCase(unittest.TestCase):
         sub_test_env = env._Env(param, args, outer=test_env)
         self.assertEqual(sub_test_env.find('a'), sub_test_env)
         self.assertEqual(sub_test_env.find('x'), test_env)
+
+        params = ['x', '.', 'y']
+        args = [1, 2, 3, 4, 5]
+        test_env = env._Env(params, args)
+        self.assertEqual(test_env['x'], 1)
+        self.assertEqual(test_env['y'], [2, 3, 4, 5])
 
     def test_standard_env(self):
         self.assertIsInstance(env._global_env, env._Env)
