83ab2221087be80a1f945873ccf7a2e6abd05f33
Takahiro Ishikawa <takahish.nil@gmail.com>
2018-06-22 01:01:16 +0900

    Added types/number.py

---
+++ clispy/types/number.py
@@ -0,0 +1,349 @@
+# Copyright 2018 Takahiro Ishikawa. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import operator as op
+from fractions import Fraction
+import numpy as np
+from clispy.types.t import T
+
+
+class Number(T):
+    """The type Number contains objects which represent mathematical numbers.
+    The types Real and Complex are disjoint subtypes of Number.
+    """
+    @staticmethod
+    def check_type(obj):
+        """Check whether an object is an instance of Number.
+
+        Args:
+            obj: An object.
+        """
+        if not isinstance(obj, Number):
+            raise TypeError("The value " + str(obj) + " is not of type clispy.types.Number")
+
+    def __eq__(self, other):
+        """Hook of self == other.
+        """
+        return Number.__compare(op.eq, self, other)
+
+    def __ne__(self, other):
+        """Hook of self != other.
+        """
+        return Number.__compare(op.ne, self, other)
+
+    def __lt__(self, other):
+        """Hook of self < other.
+        """
+        return Number.__compare(op.lt, self, other)
+
+    def __le__(self, other):
+        """Hook of self <= other.
+        """
+        return Number.__compare(op.le, self, other)
+
+    def __gt__(self, other):
+        """Hook of self > other.
+        """
+        return Number.__compare(op.gt, self, other)
+
+    def __ge__(self, other):
+        """Hook of self >= other.
+        """
+        return Number.__compare(op.ge, self, other)
+
+    @staticmethod
+    def __compare(operator, self, other):
+        """Compare two numbers.
+        """
+        Number.check_type(other)
+        return operator(self.value, other.value)
+
+
+class Real(Number):
+    """The type Real includes all numbers represent mathematical real numbers,
+    though there are mathematical real numbers (e.g., irrational numbers)
+    that do not have an exact representation in Common Lisp. Only can be
+    ordered using the <, >, <=, and >= functions.
+    The types Rational and Float are disjoint subtypes of type Real
+    """
+    pass
+
+
+class Rational(Real):
+    """The canonical representation of a rational is as an Integer if its
+    value is integral, and otherwise as a Ratio.
+    """
+    pass
+
+
+class Integer(Rational):
+    """An integer is a mathematical integer. There is no limit on the
+    magnitude of an integer.
+    """
+    def __init__(self, value):
+        """Initialize Integer. A value is converted into np.int.
+
+        Args:
+            value: Int. It could be converted into np.int.
+        """
+        # self._value is protected.
+        self._value = np.int(value)
+
+    def __repr__(self):
+        """The official string representation.
+        """
+        return str(self.value)
+
+    def __add__(self, other):
+        """Hook of self + other.
+        """
+        return Integer.__calculate(op.add, self, other)
+
+    def __sub__(self, other):
+        """Hook of self - other.
+        """
+        return Integer.__calculate(op.sub, self, other)
+
+    def __mul__(self, other):
+        """Hook of self * other.
+        """
+        return Integer.__calculate(op.mul, self, other)
+
+    def __truediv__(self, other):
+        """Hook of self / other.
+        """
+        if isinstance(other, Integer):
+            return Ratio(str(self.value) + '/' + str(other.value))
+        else:
+            return Integer.__calculate(op.truediv, self, other)
+
+    @staticmethod
+    def __calculate(operator, self, other):
+        """Calculate arithmetic.
+        """
+        Number.check_type(other)
+        if isinstance(other, Ratio):
+            fraction = operator(self.value, other.value)
+            return Ratio(str(fraction))
+        else:
+            skeleton = type(other)
+            return skeleton(operator(self.value, other.value))
+
+
+class Fixnum(Integer):
+    """Exactly which integers are fixnums is implementation-defined.
+    """
+    def __init__(self, value):
+        """Initialize Fixnum. A value is converted into np.int16.
+
+        Args:
+            value: Int. It could be converted into np.int16.
+        """
+        # self._value is protected.
+        self._value = np.int16(value)
+
+
+class Bignum(Integer):
+    """The type bignum is defined to be exactly (and integer (not fixnum)).
+    """
+    def __init__(self, value):
+        """Initialize Bignum. A value is converted into np.int.
+
+        Args:
+            value: Int. It could be converted into np.int.
+        """
+        # self._value is protected.
+        self._value = np.int(value)
+
+
+class Ratio(Rational):
+    """A ratio is a number representing the mathematical ratio of two integers,
+    the numerator and denominator, whose greatest common divisor is one, and
+    of which the denominator is positive and greater than one.
+    """
+    def __new__(cls, ratio):
+        """Called to create a new instance of class cls.
+
+        Args:
+            ratio: String
+
+        Returns:
+            An instance.
+        """
+        if '/' not in ratio:          # An argument is ['2'].
+            return Integer(ratio[0])
+        else:                         # An argument is ['10/5'].
+            numerator, denominator = (int(i) for i in ratio.split('/'))
+            if numerator % denominator == 0:
+                return Integer(numerator // denominator)
+            else:
+                return super().__new__(cls)
+
+    def __init__(self, ratio):
+        """Initialize Ratio. A value is converted into Fraction.
+
+        Args:
+            numerator: Int.
+            denominator: Int.
+        """
+        # self._value is protected.
+        self._value = Fraction(ratio)
+
+    def __repr__(self):
+        """The official string representation.
+        """
+        return str(self.value.numerator) + "/" + str(self.value.denominator)
+
+    def __add__(self, other):
+        """Hook of self + other.
+        """
+        return Ratio.__calculate(op.add, self, other)
+
+    def __sub__(self, other):
+        """Hook of self - other.
+        """
+        return Ratio.__calculate(op.sub, self, other)
+
+    def __mul__(self, other):
+        """Hook of self * other
+        """
+        return Ratio.__calculate(op.mul, self, other)
+
+    def __truediv__(self, other):
+        """Hook of self / other
+        """
+        return Ratio.__calculate(op.truediv, self, other)
+
+    @staticmethod
+    def __calculate(operator, self, other):
+        """Calculate arithmetic.
+        """
+        Number.check_type(other)
+        if isinstance(other, Float):
+            skeleton = type(other)
+            return skeleton(operator(self.value, other.value))
+        else:
+            fraction = operator(self.value, other.value)
+            return Ratio(str(fraction))
+
+
+class Float(Real):
+    """A float is a mathematical rational (but not a Common Lisp Rational).
+    """
+    def __init__(self, value):
+        """Initialize Float. A value is converted into np.float.
+
+        Args:
+            value: Float.
+        """
+        # self._value is protected.
+        self._value = np.float(value)
+
+    def __repr__(self):
+        """The official string representation.
+        """
+        return str(self.value)
+
+    def __add__(self, other):
+        """Hook of self + other.
+        """
+        return Float.__calculate(op.add, self, other)
+
+    def __sub__(self, other):
+        """Hook of self - other.
+        """
+        return Float.__calculate(op.sub, self, other)
+
+    def __mul__(self, other):
+        """Hook of self * other.
+        """
+        return Float.__calculate(op.mul, self, other)
+
+    def __truediv__(self, other):
+        """Hook of self / other.
+        """
+        return Float.__calculate(op.truediv, self, other)
+
+    @staticmethod
+    def __calculate(operator, self, other):
+        """Calculate arithmetic.
+        """
+        Number.check_type(other)
+        skeleton = type(self)
+        return skeleton(operator(self.value, other.value))
+
+
+class ShortFloat(Float):
+    """For the four defined subtypes of type float, it is true that intermediate
+    between the type short-float and the type long-float are the type single-float
+    and the type double-float. The precise definition of these categories is
+    implementation-defined.
+    """
+    def __init__(self, value):
+        """Initialize ShortFloat. A value is converted into np.float16.
+
+        Args:
+            value: Float.
+        """
+        # self._value is protected.
+        self._value = np.float16(value)
+
+
+class SingleFloat(Float):
+    """For the four defined subtypes of type float, it is true that intermediate
+    between the type short-float and the type long-float are the type single-float
+    and the type double-float. The precise definition of these categories is
+    implementation-defined.
+    """
+    def __init__(self, value):
+        """Initialize SingleFloat. A value is converted into np.float32.
+
+        Args:
+            value: Float.
+        """
+        # self._value is protected.
+        self._value = np.float32(value)
+
+
+class DoubleFloat(Float):
+    """For the four defined subtypes of type float, it is true that intermediate
+    between the type short-float and the type long-float are the type single-float
+    and the type double-float. The precise definition of these categories is
+    implementation-defined.
+    """
+    def __init__(self, value):
+        """Initialize DoubleFloat. A value is converted into np.float64.
+
+        Args:
+            value: Float.
+        """
+        # self._value is protected.
+        self._value = np.float64(value)
+
+
+class LongFloat(Float):
+    """For the four defined subtypes of type float, it is true that intermediate
+    between the type short-float and the type long-float are the type single-float
+    and the type double-float. The precise definition of these categories is
+    implementation-defined.
+    """
+    def __init__(self, value):
+        """Initialize DoubleFloat. A value is converted into np.float128.
+
+        Args:
+            value: Float.
+        """
+        # self._value is protected.
+        self._value = np.float128(value)
---
+++ test/test_types/test_number.py
@@ -0,0 +1,504 @@
+# Copyright 2018 Takahiro Ishikawa. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import unittest
+from clispy.types.number import *
+
+
+class UnitTestCase(unittest.TestCase):
+    def testNumber(self):
+        n = Number('NUMBER')
+        self.assertIsInstance(n, T)
+        self.assertIsInstance(n, Number)
+        self.assertEqual(n.value, 'NUMBER')
+
+    def testNumberCheck(self):
+        self.assertRaisesRegex(TypeError, "The value True is not of type clispy.types.Number", Number.check_type, True)
+
+    def testReal(self):
+        r = Real('REAL')
+        self.assertIsInstance(r, T)
+        self.assertIsInstance(r, Number)
+        self.assertIsInstance(r, Real)
+        self.assertEqual(r.value, 'REAL')
+
+    def testRational(self):
+        r = Rational('RATIONAL')
+        self.assertIsInstance(r, T)
+        self.assertIsInstance(r, Number)
+        self.assertIsInstance(r, Real)
+        self.assertIsInstance(r, Rational)
+        self.assertEqual(r.value, 'RATIONAL')
+
+    def testInteger(self):
+        i = Integer(100)
+        self.assertIsInstance(i, T)
+        self.assertIsInstance(i, Number)
+        self.assertIsInstance(i, Real)
+        self.assertIsInstance(i, Rational)
+        self.assertIsInstance(i, Integer)
+        self.assertIsInstance(i.value, np.int)
+        self.assertEqual(i.value, np.int(100))
+
+    def testFixnum(self):
+        f = Fixnum(100)
+        self.assertIsInstance(f, T)
+        self.assertIsInstance(f, Number)
+        self.assertIsInstance(f, Real)
+        self.assertIsInstance(f, Rational)
+        self.assertIsInstance(f, Integer)
+        self.assertIsInstance(f, Fixnum)
+        self.assertIsInstance(f.value, np.int16)
+        self.assertEqual(f.value, np.int16(100))
+
+    def testBignum(self):
+        b = Bignum(100)
+        self.assertIsInstance(b, T)
+        self.assertIsInstance(b, Number)
+        self.assertIsInstance(b, Real)
+        self.assertIsInstance(b, Rational)
+        self.assertIsInstance(b, Integer)
+        self.assertIsInstance(b, Bignum)
+        self.assertIsInstance(b.value, np.int)
+        self.assertEqual(b.value, np.int(100))
+
+    def testRaito(self):
+        r = Ratio('2/3')
+        self.assertIsInstance(r, T)
+        self.assertIsInstance(r, Number)
+        self.assertIsInstance(r, Real)
+        self.assertIsInstance(r, Rational)
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(2, 3))
+
+    def testFloat(self):
+        f = Float(100)
+        self.assertIsInstance(f, T)
+        self.assertIsInstance(f, Number)
+        self.assertIsInstance(f, Real)
+        self.assertIsInstance(f, Float)
+        self.assertIsInstance(f.value, np.float)
+        self.assertEqual(f.value, np.float(100))
+
+    def testShortFloat(self):
+        f = ShortFloat(100)
+        self.assertIsInstance(f, T)
+        self.assertIsInstance(f, Number)
+        self.assertIsInstance(f, Real)
+        self.assertIsInstance(f, Float)
+        self.assertIsInstance(f, ShortFloat)
+        self.assertIsInstance(f.value, np.float16)
+        self.assertEqual(f.value, np.float16(100))
+
+    def testSingleFloat(self):
+        f = SingleFloat(100)
+        self.assertIsInstance(f, T)
+        self.assertIsInstance(f, Number)
+        self.assertIsInstance(f, Real)
+        self.assertIsInstance(f, Float)
+        self.assertIsInstance(f, SingleFloat)
+        self.assertIsInstance(f.value, np.float32)
+        self.assertEqual(f.value, np.float32(100))
+
+    def testDoubleFloat(self):
+        f = DoubleFloat(100)
+        self.assertIsInstance(f, T)
+        self.assertIsInstance(f, Number)
+        self.assertIsInstance(f, Real)
+        self.assertIsInstance(f, Float)
+        self.assertIsInstance(f, DoubleFloat)
+        self.assertIsInstance(f.value, np.float64)
+        self.assertEqual(f.value, np.float64(100))
+
+    def testLongFloat(self):
+        f = LongFloat(100)
+        self.assertIsInstance(f, T)
+        self.assertIsInstance(f, Number)
+        self.assertIsInstance(f, Real)
+        self.assertIsInstance(f, Float)
+        self.assertIsInstance(f, LongFloat)
+        self.assertIsInstance(f.value, np.float128)
+        self.assertEqual(f.value, np.float128(100))
+
+    def testIntegerArithmetic(self):
+        a = Integer(1)
+        b = Integer(2)
+        c = Ratio('2/3')
+        d = Float(4)
+
+        eps = 0.000000000000001
+
+        i = a + b
+        self.assertIsInstance(i, Integer)
+        self.assertEqual(i.value, np.int(3))
+
+        i = a - b
+        self.assertIsInstance(i, Integer)
+        self.assertEqual(i.value, np.int(-1))
+
+        i = a * b
+        self.assertIsInstance(i, Integer)
+        self.assertEqual(i.value, np.int(2))
+
+        r = a / b
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 2))
+
+        r = a + c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(5, 3))
+
+        r = a - c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 3))
+
+        r = a * c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(2, 3))
+
+        r = a / c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(3, 2))
+
+        f = a + d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(5.0)) < eps)
+
+        f = a - d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(-3.0)) < eps)
+
+        f = a * d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(4.0)) < eps)
+
+        f = a / f
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(0.25)) < eps)
+
+    def testIntegerArithmetic_skeleton(self):
+        a = Fixnum(1)
+        b = Fixnum(2)
+        c = Ratio('2/3')
+        d = SingleFloat(4)
+
+        eps = 0.000001
+
+        i = a + b
+        self.assertIsInstance(i, Fixnum)
+        self.assertEqual(i.value, np.int16(3))
+
+        i = a - b
+        self.assertIsInstance(i, Fixnum)
+        self.assertEqual(i.value, np.int16(-1))
+
+        i = a * b
+        self.assertIsInstance(i, Fixnum)
+        self.assertEqual(i.value, np.int16(2))
+
+        r = a / b
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 2))
+
+        r = a + c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(5, 3))
+
+        r = a - c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 3))
+
+        r = a * c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(2, 3))
+
+        r = a / c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(3, 2))
+
+        f = a + d
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(5.0)) < eps)
+
+        f = a - d
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(-3.0)) < eps)
+
+        f = a * d
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(4.0)) < eps)
+
+        f = a / f
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(0.25)) < eps)
+
+    def testIntegerCompare(self):
+        a = Integer('10')
+        b = Integer('20')
+        c = Integer('10')
+
+        self.assertTrue(a == c)
+        self.assertFalse(a == b)
+
+        self.assertTrue(a != b)
+        self.assertFalse(a != c)
+
+        self.assertTrue(a < b)
+        self.assertFalse(b < a)
+        self.assertFalse(a < c)
+
+        self.assertTrue(a <= b)
+        self.assertFalse(b <= a)
+        self.assertTrue(a <= c)
+
+        self.assertTrue(b > a)
+        self.assertFalse(a > b)
+        self.assertFalse(a > c)
+
+        self.assertTrue(b >= a)
+        self.assertFalse(a >= b)
+        self.assertTrue(a >= c)
+
+    def testRatioArithmetic(self):
+        a = Ratio('1/2')
+        b = Ratio('1/4')
+        c = Integer('1')
+        d = Float('1.0')
+
+        eps = 0.000000000000001
+
+        r = a + b
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(3, 4))
+
+        r = a - b
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 4))
+
+        r = a * b
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 8))
+
+        r = b / a
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 2))
+
+        # Check returning Integer value.
+        i = a / b
+        self.assertIsInstance(i, Integer)
+        self.assertEqual(i.value, np.int(2))
+
+        r = a + c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(3, 2))
+
+        r = a - c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(-1, 2))
+
+        r = a * c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 2))
+
+        r = a / c
+        self.assertIsInstance(r, Ratio)
+        self.assertEqual(r.value, Fraction(1, 2))
+
+        # Check returning Integer value.
+        r = c / a
+        self.assertIsInstance(r, Integer)
+        self.assertEqual(r.value, np.int(2))
+
+        f = a + d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(3/2)) < eps)
+
+        f = a - d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(-1/2)) < eps)
+
+        f = a * d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(1/2)) < eps)
+
+        f = a / d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(1/2)) < eps)
+
+    def testRatioCompare(self):
+        a = Ratio('1/2')
+        b = Ratio('2/3')
+        c = Ratio('1/2')
+
+        self.assertTrue(a == c)
+        self.assertFalse(a == b)
+
+        self.assertTrue(a != b)
+        self.assertFalse(a != c)
+
+        self.assertTrue(a < b)
+        self.assertFalse(b < a)
+        self.assertFalse(a < c)
+
+        self.assertTrue(a <= b)
+        self.assertFalse(b <= a)
+        self.assertTrue(a <= c)
+
+        self.assertTrue(b > a)
+        self.assertFalse(a > b)
+        self.assertFalse(a > c)
+
+        self.assertTrue(b >= a)
+        self.assertFalse(a >= b)
+        self.assertTrue(a >= c)
+
+    def testFloatArithmetic(self):
+        a = Float('1.0')
+        b = Float('2.0')
+        c = Ratio('2/3')
+        d = Integer('4')
+
+        eps = 0.000000000000001
+
+        f = a + b
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(3.0)) < eps)
+
+        f = a - b
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(-1.0)) < eps)
+
+        f = a * b
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(2.0)) < eps)
+
+        f = a / b
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(0.5)) < eps)
+
+        f = a + c
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(5/3)) < eps)
+
+        f = a - c
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(1/3)) < eps)
+
+        f = a * c
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(2/3)) < eps)
+
+        f = a / c
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(1.5)) < eps)
+
+        f = a + d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(5.0)) < eps)
+
+        f = a - d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(-3.0)) < eps)
+
+        f = a * d
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(4.0)) < eps)
+
+        f = a / f
+        self.assertIsInstance(f, Float)
+        self.assertTrue(abs(f.value - np.float(0.25)) < eps)
+
+    def testFloatArithmetic_skeleton(self):
+        a = SingleFloat('1.0')
+        b = SingleFloat('2.0')
+        c = Ratio('2/3')
+        d = Fixnum('4')
+
+        eps = 0.000001
+
+        f = a + b
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(3.0)) < eps)
+
+        f = a - b
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(-1.0)) < eps)
+
+        f = a * b
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(2.0)) < eps)
+
+        f = a / b
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(0.5)) < eps)
+
+        f = a + c
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(5/3)) < eps)
+
+        f = a - c
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(1/3)) < eps)
+
+        f = a * c
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(2/3)) < eps)
+
+        f = a / c
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(1.5)) < eps)
+
+        f = a + d
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(5.0)) < eps)
+
+        f = a - d
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(-3.0)) < eps)
+
+        f = a * d
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(4.0)) < eps)
+
+        f = a / f
+        self.assertIsInstance(f, SingleFloat)
+        self.assertTrue(abs(f.value - np.float32(0.25)) < eps)
+
+    def testFloatCompare(self):
+        a = Float('10.0')
+        b = Float('20.0')
+        c = Float('10.0')
+
+        self.assertTrue(a == c)
+        self.assertFalse(a == b)
+
+        self.assertTrue(a != b)
+        self.assertFalse(a != c)
+
+        self.assertTrue(a < b)
+        self.assertFalse(b < a)
+        self.assertFalse(a < c)
+
+        self.assertTrue(a <= b)
+        self.assertFalse(b <= a)
+        self.assertTrue(a <= c)
+
+        self.assertTrue(b > a)
+        self.assertFalse(a > b)
+        self.assertFalse(a > c)
+
+        self.assertTrue(b >= a)
+        self.assertFalse(a >= b)
+        self.assertTrue(a >= c)
