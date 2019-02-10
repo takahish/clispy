1e21af7750a93e7cebf6458bf92e2fb17c44a9a0
Takahiro Ishikawa <takahish.nil@gmail.com>
2019-02-10 22:06:59 +0900

    fix: Fixed Cons class and added Cons.tocons method
    Fixed Cons class and added Cons.tocons method to create dotted list from
    python list.

--- clispy/type/sequence.py
+++ clispy/type/sequence.py
@@ -51,113 +51,6 @@ class List(Sequence):
         return BuiltInClass.get_instance(cls, 'LIST', True)
 
 
-class Cons(List):
-    """A Cons is compound object having two object called car and cdr.
-    Each component can be any object.
-    """
-    def __new__(cls, *args, **kwargs):
-        """Instantiates Cons. If an instance of Cons is already existed
-        in object_table, returns the instance. Otherwise, a new instance is made.
-        """
-        if len(args) == 1 and isinstance(args[0], list) and len(args[0]) == 0:
-            return Null()  # when Cons([]) is executed
-        else:
-            return BuiltInClass.get_instance(cls, 'CONS', *args)
-
-    def __init__(self, car, cdr=None):
-        """Initializes Cons.
-
-        Args:
-            car: mix.
-            cdr: Cons.
-        """
-        if isinstance(car, list):
-            if len(car) == 0:   # when Cons([], `something`) is executed
-                self.car = Null()
-                self.cdr = cdr
-            elif len(car) > 0:  # when Cons([1, 2], `something`) is executed
-                if cdr is None:
-                    cons = Cons._list_to_cons(car, Null())
-                    self.car = cons.car
-                    self.cdr = cons.cdr
-                else:
-                    cons = Cons._list_to_cons(car, Null())
-                    self.car = cons
-                    self.cdr = cdr
-        else:
-            if cdr is None:
-                cdr = Null()
-            self.car = car
-            self.cdr = cdr
-
-        # sets value as list
-        self.value = self.tolist()
-
-    def __repr__(self):
-        """The official string representation.
-        """
-        return '(' + Cons._repr_helper(self).strip() + ')'
-
-    def tolist(self):
-        """Returns a python list.
-        """
-        return Cons._cons_to_list(self, [])
-
-    @classmethod
-    def _repr_helper(cls, cons):
-        """Helper function for the official string representation.
-        """
-        if isinstance(cons, Null):        # end of cons
-            return ''
-        elif not isinstance(cons, Cons):  # for dotted pair
-            return '. ' + str(cons)
-        else:
-            return str(cons.car) + ' ' + cls._repr_helper(cons.cdr)
-
-    @classmethod
-    def _cons_to_list(cls, cons, acc):
-        """Helper function for tolist.
-
-        Args:
-            cons: clispy.type.sequence.Cons
-            acc: list. an accumulator
-
-        Returns:
-            list
-        """
-        if isinstance(cons, Null):        # end of cons
-            return acc
-        elif not isinstance(cons, Cons):  # for dotted pair
-            acc.append(cons)
-            return acc
-        else:
-            car = cons.car
-            if not isinstance(car, Cons):
-                acc.append(car)
-            else:                         # when car is instance of Cons
-                acc.append(cls._cons_to_list(cons.car, []))
-            return cls._cons_to_list(cons.cdr, acc)
-
-    @classmethod
-    def _list_to_cons(cls, lst, cons):
-        """Helper function for constructor.
-
-        Args:
-            lst: list
-            cons: clispy.type.sequence.Cons or clispy.type.sequence.Null
-
-        Returns:
-            clispy.type.sequence.Cons
-        """
-        if len(lst) == 0:                 # end of list
-            return cons
-        else:
-            if not isinstance(lst[-1], list):
-                return cls._list_to_cons(lst[:-1], Cons(lst[-1], cons))
-            else:                         # when an element is nested list
-                return cls._list_to_cons(lst[:-1], Cons(cls._list_to_cons(lst[-1], Null()), cons))
-
-
 class Null(Symbol, List):
     """The only object of type null is nil, which represents the empty
     list and can also be notated ().
@@ -181,3 +74,118 @@ class Null(Symbol, List):
         """The official string representation.
         """
         return str(self.value)
+
+
+class Cons(List):
+    """A Cons is compound object having two object called car and cdr.
+    Each component can be any object.
+    """
+    def __new__(cls, *args, **kwargs):
+        """Instantiates Cons. If an instance of Cons is already existed
+        in object_table, returns the instance. Otherwise, a new instance is made.
+        """
+        if len(args) == 1 and isinstance(args[0], list) and len(args[0]) == 0:
+            return Null()  # when Cons([]) is executed
+        else:
+            return BuiltInClass.get_instance(cls, 'CONS', *args)
+
+    def __init__(self, car, cdr=Null()):
+        """Initializes Cons.
+
+        Args:
+            car: mix.
+            cdr: Cons.
+        """
+        # If car is list, converted into Cons.
+        if isinstance(car, list):
+            if len(car) == 0:
+                car = Null()
+            else:
+                car = Cons.tocons(car)
+
+        # If cdr is list, converted into Cons.
+        if isinstance(cdr, list):
+            if len(cdr) == 0:
+                cdr = Null()
+            else:
+                cdr = Cons.tocons(cdr)
+
+        # Sets car and cdr.
+        self.car = car
+        self.cdr = cdr
+
+        # Sets value as list
+        self.value = self._tolist()
+
+    def __repr__(self):
+        """The official string representation.
+        """
+        return '(' + Cons._repr_helper(self).strip() + ')'
+
+    @classmethod
+    def _repr_helper(cls, cons):
+        """Helper function for the official string representation.
+        """
+        if isinstance(cons, Null):        # end of cons
+            return ''
+        elif not isinstance(cons, Cons):  # for dotted pair
+            return '. ' + str(cons)
+        else:
+            return str(cons.car) + ' ' + cls._repr_helper(cons.cdr)
+
+    def _tolist(self):
+        """Returns a python list.
+        """
+        return Cons._tolist_helper(self, [])
+
+    @classmethod
+    def _tolist_helper(cls, cons, acc):
+        """Helper function for tolist.
+
+        Args:
+            cons: clispy.type.sequence.Cons
+            acc: list. an accumulator
+
+        Returns:
+            list
+        """
+        if isinstance(cons, Null):        # end of cons
+            return acc
+        elif not isinstance(cons, Cons):  # for dotted pair
+            acc.append(cons)
+            return acc
+        else:
+            car = cons.car
+            if not isinstance(car, Cons):
+                acc.append(car)
+            else:                         # when car is instance of Cons
+                acc.append(cls._tolist_helper(cons.car, []))
+            return cls._tolist_helper(cons.cdr, acc)
+
+    @classmethod
+    def tocons(cls, lst):
+        """tocons converts python list into cons.
+        """
+        if (len(lst) >= 3) and lst[-2] is Symbol('.'):
+            return cls._tocons_helper(lst[:-2], lst[-1])
+        else:
+            return cls._tocons_helper(lst, Null())
+
+    @classmethod
+    def _tocons_helper(cls, lst, cons):
+        """Helper function for constructor.
+
+        Args:
+            lst: list
+            cons: clispy.type.sequence.Cons or clispy.type.sequence.Null
+
+        Returns:
+            clispy.type.sequence.Cons
+        """
+        if len(lst) == 0:                 # end of list
+            return cons
+        else:
+            if not isinstance(lst[-1], list):
+                return cls._tocons_helper(lst[:-1], Cons(lst[-1], cons))
+            else:                         # when an element is nested list
+                return cls._tocons_helper(lst[:-1], Cons(cls.tocons(lst[-1]), cons))
