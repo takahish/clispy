e64ae6848cf42eda7dbbac7486da4a9fd1b57e27
Takahiro Ishikawa <takahish.nil@gmail.com>
2019-03-31 22:10:30 +0900

    feat: Added function of importing python module
    Added function of importing python module to manipulate python objects.

--- clispy/python/object_manipulation.py
+++ clispy/python/object_manipulation.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 # ==============================================================================
 
+from importlib import import_module
 from clispy.function import SystemFunction
-from clispy.package import assign_helper
+from clispy.package import PackageManager, assign_helper
 from clispy.python import PyObject
-from clispy.type import Cons, Null, T
+from clispy.type import Cons, Null, String, T
 
 
 # ==============================================================================
@@ -43,8 +44,43 @@ class ObjectManipulation(SystemFunction):
 # ==============================================================================
 # Defines object manipulation classes
 #
-#
+#     ImportModule  Call
 # ==============================================================================
+
+class ImportModuleObjectManipulation(ObjectManipulation):
+    """Assigns modules to variables like setq special operator.
+    """
+    def __new__(cls, *args, **kwargs):
+        """Instantiates ImportModuleObjectManipulation.
+        """
+        cls.__name__ = 'IMPORT-MODULE'
+        return object.__new__(cls)
+
+    def __call__(self, forms, var_env, func_env, macro_env):
+        """Behavior of ImportModuleObjectManipulation.
+        """
+        while forms is not Null():
+            symbol, module_name, forms = forms.car, forms.cdr.car, forms.cdr.cdr
+
+            symbol_name = symbol.value
+            module = PyObject(import_module(module_name.value))
+
+            # Interns symbol that represents module name into current package.
+            PackageManager.intern(String(symbol_name))
+
+            # import-moduel may be used for assignment of both lexical and dynamic variables.
+            try:
+                var_env.find(symbol_name)[symbol_name] = module
+            except LookupError:
+                try:
+                    # package_name=None means finding an environment from current package.
+                    PackageManager.find(symbol_name, package_name=None, status_check=False)[symbol_name] = module
+                except LookupError:
+                    PackageManager.current_package.env['VARIABLE'][symbol_name] = module
+
+        # the primary value of the last form
+        return symbol
+
 
 class CallObjectManipulation(ObjectManipulation):
     """Call python object with message (method) and args.
@@ -73,7 +109,7 @@ class CallObjectManipulation(ObjectManipulation):
             py_args = []
 
             while rest_args is not Null():
-                py_args.append(rest_args.car)
+                py_args.append(rest_args.car.value)
                 rest_args = rest_args.cdr
 
             return PyObject(attr(*py_args))
@@ -86,4 +122,5 @@ class CallObjectManipulation(ObjectManipulation):
 # Set functions related on object manipulation
 # ==============================================================================
 
+assign_helper(symbol_name='IMPORT-MODULE', value=ImportModuleObjectManipulation(), package_name='PYTHON', env='FUNCTION', status=':EXTERNAL')
 assign_helper(symbol_name='CALL', value=CallObjectManipulation(), package_name='PYTHON', env='FUNCTION', status=':EXTERNAL')
